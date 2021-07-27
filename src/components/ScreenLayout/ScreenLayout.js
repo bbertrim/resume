a62fa9947d9136c343107f4638819ae6706896b2
Bradley Bertrim <bertrimbradley@gmail.com>
2021-07-27 11:31:21 -0400

    Add cover-letter/about me sections

--- src/components/ScreenLayout/ScreenLayout.js
+++ src/components/ScreenLayout/ScreenLayout.js
@@ -1,4 +1,5 @@
 import PrintHeader from '../PrintHeader/PrintHeader';
+import signatureImage from '../../images/signature.png';
 
 export const ScreenLayout = props => {
 
@@ -6,6 +7,17 @@ export const ScreenLayout = props => {
         <screenlayout>
             <content>
                 <PrintHeader />
+                <section id="about-me">
+                    <h1>About Me</h1>
+                    <p>I am a motivated and enterprising individual. I’m especially drawn to expanding my knowledge and abilities by investigating innovative technologies and approaches to challenges and problems that I’m presented with. This has led me to have a very diversified skill set that continues to expand and allows me to bring a large amount of experience and perspective when working on solutions.</p>
+                    <p>I bring with me experience working in and around the entirety of the development lifecycle, from conception and requirements gathering to launching and supporting the application. Additionally, I have experience working in multiple development and production environments/stacks. This includes Linux based systems running on bare metal, in Virtual Machines, Docker Containers running in Kubernetes as well applications deployed to cloud based services. Each with specific stacks for the given solution, be it lightweight micro services or larger web applications such as Liferay and Drupal.</p>
+                    <p>Additionally, I have an interest in creative fields such as: Art, Writing and Game Design. This has led me to carry my art education on with Digital Painting/Drawing as a personal hobby. This has also led me to start up my own company, Scribulations Inc. in order to license and sell game assets.</p>
+                    <signature>
+                        <p>Sincerely,</p>
+                        <img src={signatureImage} alt="Signature: Bradley Bertrim" />
+                        <p>Bradley Bertrim</p>
+                    </signature>
+                </section>
                 <section>
                     <h1>Experience</h1>
                     <institution>
